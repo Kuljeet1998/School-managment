ecf562785e2d7ef0a9ddf9ac27415c1e444982e2
Kuljeet <kbhengura@gmail.com>
2020-08-26 19:13:31 +0530

    Test cases

--- classroom/models.py
+++ classroom/models.py
@@ -5,26 +5,26 @@ from django.contrib.auth.models import User
 
 class Teacher(models.Model):
 	staff_no=models.IntegerField(primary_key=True)
-	user=models.OneToOneField(User ,related_name="teacher", on_delete=models.CASCADE)
+	user=models.OneToOneField(User ,related_name="teachers", on_delete=models.CASCADE)
 
 
 	def __str__(self):
-		return self.user.first_name+" (teacher)"
+		return self.user.first_name
 
 class Student(models.Model):
 	roll_no=models.IntegerField(primary_key=True)
-	user=models.OneToOneField(User ,related_name="student", on_delete=models.CASCADE)
+	user=models.OneToOneField(User ,related_name="students", on_delete=models.CASCADE)
 
 
 	def __str__(self):
-		return self.user.first_name+" (student)"
+		return self.user.first_name
 
 class Course(models.Model):
 	number=models.IntegerField()
 	name=models.CharField(max_length=20)
 
-	teacher=models.ManyToManyField(Teacher, related_name="course")
-	members=models.ManyToManyField(Student,through='StudentCourse', related_name="course")
+	teacher=models.ManyToManyField(Teacher, related_name="courses")
+	members=models.ManyToManyField(Student,through='StudentCourse', related_name="courses")
 
 	def __str__(self):
 		return self.name
@@ -33,16 +33,16 @@ class StudentCourse(models.Model):
 	APPROVE=((0,"Pending"),(1,"Approved"))
 	student=models.ForeignKey(Student,on_delete=models.CASCADE)
 	course=models.ForeignKey(Course,on_delete=models.CASCADE)
-	is_approved=models.IntegerField(choices=APPROVE,default=0)
+	approval_status=models.IntegerField(choices=APPROVE,default=0)
 
 class Assessment(models.Model):
 	title=models.CharField(max_length=15)
 	marks_obtained=models.IntegerField()
 	total_marks=models.IntegerField()
 
-	teacher=models.ForeignKey(Teacher,on_delete=models.CASCADE,related_name="assessment")
-	course=models.ForeignKey(Course,on_delete=models.CASCADE,related_name="assessment")
-	student=models.ForeignKey(Student,on_delete=models.CASCADE,related_name="assessment")
+	teacher=models.ForeignKey(Teacher,on_delete=models.CASCADE,related_name="assessments")
+	course=models.ForeignKey(Course,on_delete=models.CASCADE,related_name="assessments")
+	student=models.ForeignKey(Student,on_delete=models.CASCADE,related_name="assessments")
 
 	def __str__(self):
 		percentage=round((self.marks_obtained/self.total_marks)*100,2)
--- classroom/tests.py
+++ classroom/tests.py
@@ -1,13 +1,58 @@
 from django.test import TestCase
-from .models import *
+from classroom.models import *
+from django.contrib.auth.models import User
+# import unittest
 # Create your tests here.
-class AssessmentTestCase(TestCase):
-	def setup(self):
-		Assessment.objects.create(title="Sem1", marks_obtained=14, total_marks=20,teacher=Teacher.objects.get(satff_no=2),student=Student.objects.get(roll_no=1),course=Course.objects.get(id=3))
-		Assessment.objects.create(title="Sem2",marks_obtained=12,total_marks=20,teacher=Teacher.objects.get(staff_no=2),student=Student.objects.get(roll_no=1),course=Course.objects.get(id=3))
 
-	def display_percent(self):
+class UserTestCase(TestCase):
+
+	def setUp(self):
+		user=User.objects.create_user('foo', password='bar')
+		user.is_superuser=False
+		user.is_staff=True
+		user.save()
+		user=User.objects.first()
+
+
+	def test_display_user(self):
+		user=User.objects.get(username="foo")
+		print(user)	
+		self.assertEqual(user.username,"foo")
+
+
+
+
+class AssessmentTestCase(TestCase):
+
+	def setUp(self):
+		user=User.objects.create_user('foo', password='bar')
+		user.is_superuser=False
+		user.is_staff=True
+		user.save()
+		user=User.objects.first()
+
+		Teacher.objects.create(staff_no=1,user=user)
+		Student.objects.create(roll_no=1,user=user)
+
+		teacher=Teacher.objects.first()
+		student=Student.objects.first()
+
+		Course.objects.create(number=1,name="TEST COURSE")
+		course=Course.objects.first()
+		course.teacher.add(teacher)
+		StudentCourse.objects.create(student=student,course=course,is_approved=1)
+		course.members.add(student)
+
+		Assessment.objects.create(title="Sem1", marks_obtained=14, total_marks=20,teacher=teacher,student=student,course=course)
+		Assessment.objects.create(title="Sem2",marks_obtained=12,total_marks=20,teacher=teacher,student=student,course=course)
+
+	def test_display_percent(self):
 		sem1=Assessment.objects.get(title="Sem1")
 		sem2=Assessment.objects.get(title="Sem2")
 		print(sem1)
 		print(sem2)
+		title1=sem1.title
+		title2=sem2.title
+		print(title1+"  \t "+ title2)
+		self.assertEqual(title1, 'Sem1')
+		self.assertEqual(title2, 'Sem2')
