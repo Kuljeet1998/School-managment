85ce3c64c1c8c6091ecb81a9a22a9d90c1fa7278
Kuljeet <kbhengura@gmail.com>
2020-08-26 15:35:20 +0530

    Added APIs

--- classroom/tests.py
+++ classroom/tests.py
@@ -3,11 +3,11 @@ from .models import *
 # Create your tests here.
 class AssessmentTestCase(TestCase):
 	def setup(self):
-		Assessment.objects.create(title="Sem1", marks_obtained=14, total_marks=20,teacher=Teacher.objects.get(id=2),student=Student.objects.get(id=1),course=Course.objects.get(id=3))
-		Assessment.objects.create(title="Sem2",marks_obtained=12,total_marks=20,teacher=Teacher.objects.get(id=2),student=Student.objects.get(id=1),course=Course.objects.get(id=3))
+		Assessment.objects.create(title="Sem1", marks_obtained=14, total_marks=20,teacher=Teacher.objects.get(satff_no=2),student=Student.objects.get(roll_no=1),course=Course.objects.get(id=3))
+		Assessment.objects.create(title="Sem2",marks_obtained=12,total_marks=20,teacher=Teacher.objects.get(staff_no=2),student=Student.objects.get(roll_no=1),course=Course.objects.get(id=3))
 
 	def display_percent(self):
 		sem1=Assessment.objects.get(title="Sem1")
 		sem2=Assessment.objects.get(title="Sem2")
-		# self.assertEqual(sem1, 'RESULTS ARE HERE')
-		# self.assertEqual(sem2, 'RESULTS ARE HERE')
+		print(sem1)
+		print(sem2)
